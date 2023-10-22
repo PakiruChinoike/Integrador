2c23f41c69d81a4d4bcf598cc478faebaa37558b
bufaloow <145345469+bufaloow@users.noreply.github.com>
2023-10-21 21:33:29 -0300

    Update CatacombsIntegrador.sql

--- BD/CatacombsIntegrador.sql
+++ BD/CatacombsIntegrador.sql
@@ -55,6 +55,7 @@ CREATE TABLE item(
 id_item BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
 raridade VARCHAR(30) NOT NULL,
+ xp SMALLINT,
 id_habilidade BIGINT NOT NULL,
 PRIMARY KEY(id_item),
 FOREIGN KEY (id_habilidade) REFERENCES habilidade(id_habilidade)
