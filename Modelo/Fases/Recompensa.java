b84337aed6775ac301147d41e949e586b16c702f
PakiruChinoike <pakillbrine@gmail.com>
2023-11-27 07:58:11 -0300

    RecompensaDAO

--- Modelo/Fases/Recompensa.java
+++ Modelo/Fases/Recompensa.java
@@ -48,24 +48,48 @@ public class Recompensa{
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public int getTipo() {
         return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
     }
 
     public int getRaridade() {
         return raridade;
     }
 
+    public void setRaridade(int raridade) {
+        this.raridade = raridade;
+    }
+
     public int getExperiencia() {
         return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
     }
 
     public Item getItem() {
         return item;
     }
 
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
     public Habilidade getHabilidade() {
         return habilidade;
+    }
+
+    public void setHabilidade(Habilidade habilidade) {
+        this.habilidade = habilidade;
     }
 
     //METODO QUE TRANSFORMA TODAS AS RECOMPENSAS EM STRING
--- Persistencia/RecompensaDAO.java
+++ Persistencia/RecompensaDAO.java
@@ -6,60 +6,91 @@ public class RecompensaDAO {
 
 private ConexaoMYSQL conexao;
 
-public RecompensaDAO(){
-    this.conexao = new ConexaoMYSQL("localhost", "3306", "root", "1237353h", "CatacombsIntegrador");
-}
+    public RecompensaDAO(){
+        this.conexao = new ConexaoMYSQL("localhost", "3306", "root", "1237353h", "CatacombsIntegrador");
+    }
 
-public long salvar(Recompensa recompensa) {
-    try{
-        this.conexao.abrirConexao();
-        String sql = "INSERT INTO recompensa VALUES(null, ?, ?, ?, ?, ?)";
-        PreparedStatement statement = this.conexao.getConexao().prepareStatement(sql);
-        statement.setInt(1, recompensa.getTipo());
-        statement.setInt(2, recompensa.getExperiencia());
-        statement.setInt(3, recompensa.getRaridade());
+    public long salvar(Recompensa recompensa) {
+        try{
+            this.conexao.abrirConexao();
+            String sql = "INSERT INTO recompensa VALUES(null, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sql);
+            statement.setInt(1, recompensa.getTipo());
+            statement.setInt(2, recompensa.getExperiencia());
+            statement.setInt(3, recompensa.getRaridade());
 
-        if(recompensa.getHabilidade()!=null) {
+            if(recompensa.getHabilidade()!=null) {
+                statement.setLong(4, recompensa.getHabilidade().getId());
+            }
+            else {
+                statement.setRef(4, null);
+            }
+
+            if(recompensa.getItem()!=null){
+                statement.setLong(5, recompensa.getItem().getId());
+            }
+            else {
+                statement.setRef(5, null);
+            }
+
+            statement.executeUpdate();
+
+            String sql0 = "SELECT * FROM recompensa ORDER BY id_recompensa DESC LIMIT 1";
+            PreparedStatement statement0 = this.conexao.getConexao().prepareStatement(sql0);
+            ResultSet rs0 = statement0.executeQuery();
+
+            if (rs0.next()) {
+                long id_recompensa = rs0.getLong("id_recompensa");
+                return id_recompensa;
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return (Long)null;
+        }
+        finally {
+            this.conexao.fecharConexao();
+            return (Long)null;
+        }
+    }
+
+    public void editar(Recompensa recompensa) {
+        try {
+            this.conexao.abrirConexao();
+            String sql = "UPDATE recompensa SET tipo=?, experiencia=?, raridade=?, id_habilidade=?, id_item=? WHERE id_recompensa=?";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sql);
+            statement.setInt(1, recompensa.getTipo());
+            statement.setInt(2, recompensa.getExperiencia());
+            statement.setInt(3, recompensa.getRaridade());
             statement.setLong(4, recompensa.getHabilidade().getId());
-        }
-        else {
-            statement.setRef(4, null);
-        }
-
-        if(recompensa.getItem()!=null){
             statement.setLong(5, recompensa.getItem().getId());
-        }
-        else {
-            statement.setRef(5, null);
-        }
-
-        statement.executeUpdate();
-
-        String sql0 = "SELECT recompensa FROM recompensa ORDER BY id_recompensa DESC LIMIT 1";
-        PreparedStatement statement0 = this.conexao.getConexao().prepareStatement(sql0);
-        ResultSet rs0 = statement0.executeQuery();
-
-        if (rs0.next()) {
-            long id_recompensa = rs0.getLong("id_recompensa");
-            return id_recompensa;
+            statement.setLong(6, recompensa.getId());
+            statement.executeUpdate();
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        finally {
+            this.conexao.fecharConexao();
         }
     }
-    catch (SQLException e) {
-        e.printStackTrace();
-        return (Long)null;
+
+    public Recompensa buscar(long id) {
+        try {
+            this.conexao.abrirConexao();
+            String sql = "SELECT * FROM recompensa WHERE id_recompensa=?";
+            PreparedStatement statement = this.conexao.getConexao().prepareStatement(sql);
+            statement.setLong(1, id);
+            ResultSet rs = statement.executeQuery();
+
+            if(rs.next()) {
+                Recompensa recompensa = new Recompensa(rs.getInt("tipo"), rs.getInt("raridade"), rs.getInt("experiencia"));
+                recompensa.setId(rs.getLong("id_recompensa"));
+            }
+        }
     }
-    finally {
-        this.conexao.fecharConexao();
-    }
-}
-public void editar(Recompensa recompensa) {
-    try {
-         this.conexao.abrirConexao();
-         String sql = "UPDATE recompensa SET tipo=?, experiencia=?, raridade=? WHERE id_recompensa=?";
-          PreparedStatement statement = this.conexao.getConexao().prepareStatement(sql);
-          statement.setInt(1, recompensa.getTipo());
-          statement.setInt(2, recompensa.getExperiencia());
-          statement.setInt(3, recompensa.getRaridade());
-          statement.executeUpdate();
+
+    public void excluir(long id) {
+
     }
 }
